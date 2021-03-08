5e3e98edd1586d5e1668c49ea72a74e32c242ea6
meili <meiliham1@gmail.com>
2021-03-08 10:42:54 -0500

    commit tree 6f298be39b928d767fa0bbead1f0f50e91a532d1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+
+        @knowledge << string
+    end
+
+    def knowledge #Kim.knowledge = array of knowledge
+        @knowledge
+    end
 
 end
